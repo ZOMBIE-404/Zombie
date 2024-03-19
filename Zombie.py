465b5e3d748851b23c2dba9c3c1732d6902e1741
ZOMBIE-404 <143811328+ZOMBIE-404@users.noreply.github.com>
2024-03-19 09:41:20 +0500

    Update Zombie.py

--- Zombie.py
+++ Zombie.py
@@ -5,6 +5,6 @@ print('\033[92;1m Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/Ly6r4l7xhy445swHOckKyy')
 ZOMBIE=platform.architecture()[0]
 if ZOMBIE=="32bit":
-    __import__("FB-DEVIL32")
+    __import__("BS")
 elif ZOMBIE=="64bit":
     __import__("NS")
